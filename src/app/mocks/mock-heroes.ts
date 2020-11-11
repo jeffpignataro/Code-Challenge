18865002428fe51cad928ae6d75de805ddc9dd6c
Jeff Pignataro <jeffp2662@gmail.com>
2020-11-11 09:41:02 -0700

    Simple implementation of lists and forms

---
+++ src/app/mocks/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from '../interfaces/hero';
+
+export const HEROES: Hero[] = [
+  { id: 101, name: 'Dr Nice' },
+  { id: 102, name: 'Narco' },
+  { id: 103, name: 'Bombasto' },
+  { id: 104, name: 'Celeritas' },
+  { id: 105, name: 'Magneta' },
+  { id: 106, name: 'RubberMan' },
+  { id: 107, name: 'Dynama' },
+  { id: 108, name: 'Dr IQ' },
+  { id: 109, name: 'Magma' },
+  { id: 110, name: 'Tornado' }
+];
